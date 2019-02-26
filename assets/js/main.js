b43ec29f5010226f34e08da1baef46b50c2321e0
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-02-25 21:27:40 -0800

    added background, gif animate state

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,6 @@ const idButtons = $('#buttons');
 const idAddButt = $('#addButt');
 const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=mD36BJfT1Yf7RVO8Xqfm65rJgtck3eKI?q=cat";
 
-
-
 for (let i = 0; i < topicItems.length; i++) {
     let $butt = $('<button/>',
         {
@@ -15,10 +13,6 @@ for (let i = 0; i < topicItems.length; i++) {
         });
     idButtons.append($butt);
 }
-function alertTest() {
-    alert(`You Pressed : ${$(this)[0].firstChild.data}`);
-}
-
 
 idAddButt.click(function (event) {
     event.preventDefault();
@@ -33,22 +27,35 @@ idAddButt.click(function (event) {
 });
 
 function searchGifApi() {
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     for (var i = 0; i < 20; i++) {
-    //         var img = $('<img>').attr('src', response.data[i].images.downsized_medium.url);
-    //         $('#gifDisplay').append(img);
-    //     }
-    // })
     let searchItem = $(this)[0].firstChild.data;
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+searchItem+"&api_key=mD36BJfT1Yf7RVO8Xqfm65rJgtck3eKI&limit=5");
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+searchItem+"&api_key=mD36BJfT1Yf7RVO8Xqfm65rJgtck3eKI&limit=20");
     xhr.done(function (data) {
         console.log("success got data", data);
-        for (var i = 0; i < 20; i++) {
-            var img = $('<img>').attr('src', data.data[i].images.downsized_medium.url);
-            $('#gifDisplay').prepend(img);
+        for (let i = 0; i < data.data.length; i++) {
+            let $img = $('<img>');
+            let gifStill = data.data[i].images.downsized_still.url;
+            let gifAnimate = data.data[i].images.downsized_medium.url;
+            $img.addClass('gifImages');
+            $img.attr('data-gifAnimate',gifAnimate);
+            $img.attr('data-gifStill',gifStill);
+            $img.attr('data-gifAnimateState', 'false');
+            $img.attr('src', gifStill);
+            $img.on('click',gifState);
+            $('#gifDisplay').prepend($img);
         }
     });
 };
+
+function gifState(){
+    let gifStill = $(this).attr('data-gifStill');
+    let gifAnimate = $(this).attr('data-gifAnimate');
+    let gifState = $(this).attr('data-gifAnimateState');
+    console.log(gifState);
+    if($(this).attr('data-gifAnimateState')=='false'){
+        $(this).attr('src',gifAnimate);
+        $(this).attr('data-gifAnimateState','true');
+    } else {
+        $(this).attr('src',gifStill);
+        $(this).attr('data-gifAnimateState','false');
+    }
+}
